810963296bf4c6bfc8687d65f0359b4169b1bf48
wsq1122 <1658166398@qq.com>
2025-06-04 22:34:57 +0800

    算术运算符

---
+++ DAY 3/算数运算符.py
@@ -0,0 +1,13 @@
+"""
+算术运算符
+# Python 中的算术运算符包括加法、减法、乘法、除法、取余、幂运算等。
+Version: 1.0
+Author: Wei si qi         
+"""
+print(321+12)
+print(321-12)
+print(321*12)
+print(321/12)
+print(321//12)
+print(321%12)
+print(321**12)
