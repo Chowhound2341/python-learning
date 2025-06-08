ffe2ed48aa61d684ce5af9e1749fb1d8865346aa
Chowhound2341 <weisiqi169@gmail.com>
2025-06-08 11:56:56 +0800

    更新注释，解释海象运算符的语法错误及其用法

--- DAY 3/海象运算符.py
+++ DAY 3/海象运算符.py
@@ -3,8 +3,9 @@
 Version: 1.0
 Author: Wei si qi
 """
-#SyntaxError: invalid syntax
-#print((a = 10))
+# The following line would raise a SyntaxError because `=` cannot be used in this context:
+# The line below would raise a SyntaxError because assignment using '=' is not allowed within expressions in Python.
+#海象运算符: The walrus operator allows assignment within an expression.
 #海象运算符
 print((a := 10))
 print(a)
