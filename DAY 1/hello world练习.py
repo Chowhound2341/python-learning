24370a135646dbdc63aa28afbb2f8035c7cbb464
Chowhound2341 <weisiqi169@gmail.com>
2025-06-08 11:55:00 +0800

    重构代码，优化程序结构并添加主函数以打印“你好，世界”

--- DAY 1/hello world练习.py
+++ DAY 1/hello world练习.py
@@ -1,8 +1,17 @@
-# 第一个Python程序
-# Hello World
+"""
+第一个 Python 程序 - Hello World
 
-# version: 0.0.1
-# author: wei si qi
+版本: 0.0.1
+作者: wei si qi
+"""
 
-# print('hello world')
-print("你好，世界")
+def main() -> None:
+    """
+    主函数。
+
+    这个函数打印 "你好，世界" 到控制台。
+    """
+    print("你好，世界")
+
+if __name__ == "__main__":
+    main()
