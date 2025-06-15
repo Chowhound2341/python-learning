721877493f7f76be1d3127c161f4e3243d448918
Chowhound2341 <weisiqi169@gmail.com>
2025-06-15 19:20:31 +0800

    重构实验文件，新增列表操作示例及相关打印功能

--- DAY 1/实验.py
+++ DAY 1/实验.py
@@ -1,2 +1,17 @@
-a = 111
-print(isinstance(a, int))
+# 定义一个包含多种类型元素的列表
+my_list = ['abcd', 786, 2.23, 'runoob', 70.2]
+# 定义一个较小的列表
+tiny_list = [123, 'runoob']
+
+# 打印整个列表
+print(my_list)
+# 打印列表的第一个元素
+print(my_list[0])
+# 打印列表的第二到第三个元素（不包含第四个元素）
+print(my_list[1:3])
+# 打印列表从第三个元素开始到末尾
+print(my_list[2:])
+# 打印tiny_list列表两次
+print(tiny_list * 2)
+# 打印两个列表拼接在一起的结果
+print(my_list + tiny_list)
